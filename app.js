0af9ad828ee6f90b93a447545207c9612cebc906
kizzylion <kztchm@gmail.com>
2023-08-13 18:45:03 +0100

    IMPLEMENTED THE SCREEN CONTROLLER FUNCTION
    The screen controller function is what makes users able to interact with the game from the DOM instead of the console.
    Created an updateScreen function that prints the gameboard on the DOM and and adds an eventlistener clickHandlerBtn to each cell that help changes the value of that cell to the value associated with the player token
    Created the reset button, so we can start a new game.

--- app.js
+++ app.js
@@ -103,11 +103,15 @@ const gameController = (playerOneName = 'Player One', playerTwoName = 'Player Tw
         if(board.isCellOccupied(rowIndex,columnIndex)) return;
 
         board.setCellValue(rowIndex, columnIndex, getActivePlayer());
+        
         //this is where we check the for winner
-
         switchPlayer();
         printNewRound();
+
+       
     }
+    
+    
     printNewRound();
 
     return{
@@ -119,6 +123,66 @@ const gameController = (playerOneName = 'Player One', playerTwoName = 'Player Tw
 
 }
 
+const screenController = () => {
+    const game = gameController();
+    
 
+    const playerTurnsDiv = document.getElementById('turn');
+    const resetBtn = document.getElementById('resetBtn');
+    const boardDiv = document.getElementById('board');
 
-const game = gameController()
+    const updateScreen = ()=> {
+        boardDiv.textContent = "";
+
+        //Get the latest version of the board and player turn
+        playerTurnsDiv.textContent = `${game.getActivePlayer().name}'s Turn`;
+        const board = game.getBoard();
+
+        // Render Game value
+        board.forEach((rows, index) => {
+            let rIndex = index;
+            rows.forEach((columns, index) => {
+                let cIndex = index
+                const cellBtn = document.createElement('button');
+                cellBtn.classList.add('cell');
+                cellBtn.textContent = columns.getValue();
+                cellBtn.id =`${rIndex}${cIndex}`;
+                // add click event to cell
+                cellBtn.addEventListener("click", clickHandlerBtn);
+                boardDiv.appendChild(cellBtn);
+            })
+        })
+    }
+
+    resetBtn.addEventListener('click', resetGame)
+
+    /**
+     * Handles the click event on the cell button.
+     */
+    function clickHandlerBtn(e) {
+    // Get the cell index from the clicked button's id
+    const cellIndex = e.target.id;
+
+    // Get the row index from the cell index
+    const rowIndex = cellIndex[0];
+
+    // Get the column index from the cell index
+    const columnIndex = cellIndex[1];
+
+    // Call the game's playRound method with the row and column indices
+    game.playRound(rowIndex, columnIndex);
+    updateScreen()
+    }
+
+    // Handles the click event on the resetButton
+    function resetGame () {
+        gameBoard.resetBoard();
+        game.resetActivePlayer();
+        updateScreen();
+    }
+
+    updateScreen()
+
+}
+
+screenController()
