08b23f76b3a4e3d44dce4c268d8149586af4ec22
kizzylion <kztchm@gmail.com>
2023-08-13 15:32:21 +0100

    IMPLEMENT THE gameController Module
    Created the gameController function to handle the game logic for a Tic Tac Toe game.
    Added support for two players with customizable names and tokens.
    Implemented a function to switch players, retrieving the active player, and resetting the active player.
    make the logic to print new round

--- app.js
+++ app.js
@@ -33,43 +33,92 @@ const gameBoard = (()=> {
     }
 
     const getBoard = () => { return board };
+    const isBoardFull = () => {
+        return board.every(rows => rows.every(columns => columns.getValue() !== ""));
+    };
+
+    const isCellOccupied =(rowIndex,columnIndex) =>{
+        return board[rowIndex][columnIndex].getValue() !== "";
+    }
+    
     const setCellValue = (rowIndex, columnIndex, player) =>{
         const availableCell = board;
-        
-        // if(!availableCell.forEach(rows => rows.filter(columns => columns.getValue() === ""))) return;
-        if(availableCell[rowIndex][columnIndex].getValue() !== "") return;
         availableCell[rowIndex][columnIndex].addToken(player);
-        // console.log(availableCell.getValue)
+        console.log(availableCell[rowIndex][columnIndex].getValue())
     }
 
     const resetBoard = () => {
         board.forEach((rows)=> rows.forEach(columns => columns.resetToken()));
+        printBoard();
+        console.log(`Board Restarted`)
     }
 
     const printBoard = () => {
         const boardWithCellValue = board.map(rows => rows.map((columns) => columns.getValue()) );
-        console.log(boardWithCellValue);
+        console.table(boardWithCellValue);
     };
 
     return {
         getBoard,
+        isBoardFull,
+        isCellOccupied,
         setCellValue,
         printBoard,
         resetBoard
     };
 })()
 
-gameBoard.printBoard();
-gameBoard.setCellValue(0,2, {
-    name: 'playerOneName',
-    token: 'X'
-})
-gameBoard.setCellValue(0,2, {
-    name: 'playerOneName',
-    token: 'O'
-})
-gameBoard.printBoard();
+const gameController = (playerOneName = 'Player One', playerTwoName = 'Player Two') => {
+    const board = gameBoard;
+
+    const players = [
+        playerOne = {
+            name : playerOneName,
+            token : 'X'
+        },
+        playerTwo = {
+            name : playerTwoName,
+            token : 'O'
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayer =  () =>{
+        activePlayer = activePlayer === players[0]? players[1]: players[0];
+    }
+
+    const getActivePlayer = () => activePlayer;
+
+    const resetActivePlayer = () => activePlayer = players[0];
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s Turn...`)
+    }
+
+    const playRound = (rowIndex, columnIndex) => {
+        printNewRound();
+        if(board.isBoardFull()) return;
+        if(board.isCellOccupied(rowIndex,columnIndex)) return;
+
+        board.setCellValue(rowIndex, columnIndex, getActivePlayer());
+        //this is where we check the for winner
+
+        switchPlayer();
+        printNewRound();
+    }
+    printNewRound();
+
+    return{
+        getActivePlayer,
+        resetActivePlayer,
+        getBoard: board.getBoard,
+        playRound
+    }
+
+}
 
 
-gameBoard.resetBoard()
-gameBoard.printBoard()
+
+const game = gameController()
